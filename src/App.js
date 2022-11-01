b1d8a6d974594ad3fd83ac28c3b4d69896e14a59
Aasif Rahman <asifrahman15@gmail.com>
2022-11-01 13:46:35 +0530

    Fix: Fixing Equals and Not Equals

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
     }
 
     startTimer = (start=true) => {
-        if (start && (this.state.new_url != '')){
+        if (start && (this.state.new_url !== '')){
             clearInterval(this.timer)
             this.setState({counter: 0})
             this.timer = setInterval(() => {
@@ -27,7 +27,7 @@ class App extends React.Component {
                 else {
                     clearInterval(this.timer)
                     this.setState({counter: 0})
-                    if (this.state.new_url != ''){
+                    if (this.state.new_url !== ''){
                         this.handleSubmit()
                     }
                 }}, 1000);
@@ -45,7 +45,7 @@ class App extends React.Component {
             data.map((product, index) => {
                 data[index]['selected_image'] = 0
                 data[index]['full_description'] = false
-                if (data[index]["sizes"].length == 0){
+                if (data[index]["sizes"].length === 0){
                     data[index]["sizes"] = ["No Size Data"]
                 }
 
@@ -66,7 +66,7 @@ class App extends React.Component {
             let data = response.data
             data['selected_image'] = 0
             data['full_description'] = false
-            if (data["sizes"].length == 0){
+            if (data["sizes"].length === 0){
                 data["sizes"] = ["No Size Data"]
             }
             let product_list = [...this.state.product_list, response.data]
@@ -100,24 +100,24 @@ class App extends React.Component {
                                 <input type='text' className='form-control' value={this.state.new_url} onChange={(e) => this.setState({new_url: e.target.value}, this.startTimer)}></input>
                             </div>
                             <div className='col-lg-2 col-md-3 col-sm-4 mx-auto'>
-                                <button className='btn btn-success' disabled={this.state.fetching || this.state.new_url==''} onClick={this.handleSubmit}>Fetch Immediate {(this.state.counter==0)?"":`(${this.state.automatic_timer + 1 - this.state.counter})`}</button>
+                                <button className='btn btn-success' disabled={this.state.fetching || this.state.new_url===''} onClick={this.handleSubmit}>Fetch Immediate {(this.state.counter===0)?"":`(${this.state.automatic_timer + 1 - this.state.counter})`}</button>
                             </div>
                         </div>
                     </div>
                     <div className='card-body'>
-                        {this.state.selected_category==''?<></>:<><span className='font-weight-bold'>Selected Category:</span> <button className='btn btn-danger' onClick={() => {this.setState({selected_category: ''})}}>{this.state.selected_category} X</button></>}
+                        {this.state.selected_category===''?<></>:<><span className='font-weight-bold'>Selected Category:</span> <button className='btn btn-danger' onClick={() => {this.setState({selected_category: ''})}}>{this.state.selected_category} X</button></>}
                         <div className='row'>
                             {this.state.product_list.map((product, index) => {return <>
-                                {((this.state.selected_category=='') || (this.state.selected_category==product.category))?<>
+                                {((this.state.selected_category==='') || (this.state.selected_category===product.category))?<>
                                     <div className='col-lg-4 col-md-6 col-sm-12 mx-auto my-3' id={index}>
                                         <div className='card m-auto'>
-                                            <img className="card-img-top" src={product.images[product.selected_image]} alt="Card image"></img>
+                                            <img className="card-img-top" src={product.images[product.selected_image]} alt="Product"></img>
                                             <div className='card-header text-center'>
                                                 <div className='row'>
-                                                    <button className='btn btn-outline-secondary text-left mx-auto' disabled={product.selected_image==0} onClick={() => {this.change_image(index, -1)}}>Prev</button>
+                                                    <button className='btn btn-outline-secondary text-left mx-auto' disabled={product.selected_image===0} onClick={() => {this.change_image(index, -1)}}>Prev</button>
                                                     <button className='btn btn-outline-secondary text-center mx-auto' disabled={true}>{product.selected_image + 1} / {product.images.length}</button>
                                                     
-                                                    <button className='btn btn-outline-secondary text-right mx-auto' disabled={product.selected_image==(product.images.length-1)} onClick={() => {this.change_image(index, 1)}}>Next</button>
+                                                    <button className='btn btn-outline-secondary text-right mx-auto' disabled={product.selected_image===(product.images.length-1)} onClick={() => {this.change_image(index, 1)}}>Next</button>
                                                 </div>
                                             </div>
                                             <div className='card-header text-center font-weight-bold'>{product.title}</div>
@@ -126,7 +126,7 @@ class App extends React.Component {
                                                 <br/>
                                                 <div><span className='font-weight-bold'>Ratings: {product.rating} ⭐</span></div>
                                                 <br/>
-                                                <div><span className='font-weight-bold'>Size / Vareity: {product.sizes.map((size, size_index) => {return <>{size}{(size_index==product.sizes.length-1)?"":", "}</>})}</span></div>
+                                                <div><span className='font-weight-bold'>Size / Vareity: {product.sizes.map((size, size_index) => {return <>{size}{(size_index===product.sizes.length-1)?"":", "}</>})}</span></div>
                                                 <br/>
                                                 <div><span className='font-weight-bold'>Category: </span><button className='btn btn-success text-right' onClick={() => {this.setState({selected_category: product.category})}}>{product.category}</button></div>
                                                 <br/>
